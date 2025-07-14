1bb97838209aec125bf8ce0ff84aaab58aa251e8
Hellen Ou <hellenou@MacBookPro.hsd1.ca.comcast.net>
2025-07-14 14:36:56 -0700

    trying to deploy 2

--- server/server.js
+++ server/server.js
@@ -13,7 +13,8 @@ const port = 5001;
 
 // Middleware
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:3000", "http://127.0.0.1:5173"],
+  //origin: ["http://localhost:5173", "http://localhost:3000", "http://127.0.0.1:5173"],
+  origin: ["https://bookshelf-woad-two.vercel.app", "http://localhost:5173", "http://localhost:3000", "http://127.0.0.1:5173"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
